e02eb84a787327e9afab0865c04016d3e4893b10
Andore <andretj_92@live.com>
2020-11-12 21:08:00 -0500

    Add all instance variables to the Pokemon class

--- pokemon.rb
+++ pokemon.rb
@@ -3,7 +3,7 @@ require_relative "pokedex"
 class Pokemon
   include Pokedex
 
-  def initialize # (complete this)
+  def initialize(specie, level, name = nil)
     # Retrieve pokemon info from Pokedex and set instance variables
     # Calculate Individual Values and store them in instance variable
     # Create instance variable with effort values. All set to 0
@@ -11,6 +11,48 @@ class Pokemon
     # If level is 1, set experience points to 0 in instance variable.
     # If level is not 1, calculate the minimum experience point for that level and store it in instance variable.
     # Calculate pokemon stats and store them in instance variable
+    initialize_pokedex_variables(specie)
+    @name = name || POKEMONS[specie][:species]
+    @level = level
+    @individual_stats = { hp: rand(32), attack: rand(32), defense: rand(32), special_attack: rand(32), special_defense: rand(32), speed: rand(32) }
+    @effort_values = { hp: 0, attack: 0, defense: 0, special_attack: 0, special_defense: 0, speed: 0 } #variable
+    initialize_experience_points(@level)
+    @stats = { hp: calculate_hp, attack: calculate_else("attack"), defense: calculate_else("defense"), special_attack: calculate_else("special_attack"), special_defense: calculate_else("special_defense"), speed: calculate_else("speed") }
+  end
+
+  def initialize_pokedex_variables(specie)
+    pokemon_specie = POKEMONS[specie]
+    @specie = pokemon_specie[:species]
+    @type = pokemon_specie[:type]
+    @base_exp = pokemon_specie[:base_exp]
+    @growth_rate = pokemon_specie[:growth_rate]
+    @base_stats = pokemon_specie[:base_stats]
+    @effort_points = pokemon_specie[:effort_points]
+    @moves = pokemon_specie[:moves]
+  end
+
+  def initialize_experience_points(level)
+    if(level == 1)
+      @experience_points = 0
+    elsif level != 1 && @growth_rate == slow
+      @experience_points = (5 / 4.0 * @level**3).floor
+    elsif level != 1 && @growth_rate == medium_slow
+      @experience_points = (6 / 5.0 * @level**3 - 15 * @level**2 + 100 * @level -140).floor
+    elsif level != 1 && @growth_rate == medium_fast
+      @experience_points = @level**3
+    elsif level != 1 && @growth_rate == fast
+      @experience_points = (4 / 5.0 * @level**3).floor
+    end
+  end
+
+  def calculate_hp
+    stat_effort = (@effort_values[:hp] / 4.0).floor
+    ((2 * @base_stats[:hp] + @individual_stats[:hp] + stat_effort) * @level / 100 + @level + 10).floor  
+  end
+
+  def calculate_else(stat)
+    stat_effort = (@effort_values[stat.to_sym] / 4.0).floor
+    ((2 * @base_stats[stat.to_sym] + @individual_stats[stat.to_sym] + stat_effort) * @level / 100 + 5).floor
   end
 
   def prepare_for_battle
@@ -55,3 +97,6 @@ class Pokemon
   # private methods:
   # Create here auxiliary methods
 end
+
+prueba = Pokemon.new("Pikachu", 1)
+p prueba
